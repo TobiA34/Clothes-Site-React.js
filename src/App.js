67164b44d85c956604c153a84295983d97d6d5ab
“TobiA34” <tobiadegoroye49@gmail.com>
2024-11-23 15:34:02 +0000

    Added star rating mock

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import NavbarComponent from "./Components/NavbarComponent";
 import FilterComponent from "./Components/FilterComponent";
 import { useState } from "react";
 import Product from "./Components/Product";
- 
+
+
 function App() {
   const items = [
     {
@@ -13,60 +14,70 @@ function App() {
       name: "Item 1",
       price: 10.99,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 2,
       name: "Item 2",
       price: 12.49,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 3,
       name: "Item 3",
       price: 8.99,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 4,
       name: "Item 4",
       price: 15.99,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 5,
       name: "Item 5",
       price: 20.0,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 6,
       name: "Item 6",
       price: 7.5,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 7,
       name: "Item 7",
       price: 18.75,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 8,
       name: "Item 8",
       price: 25.0,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 9,
       name: "Item 9",
       price: 5.99,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
     {
       id: 10,
       name: "Item 10",
       price: 14.99,
       desc: "Organic Cotton, fair trade certified",
+      rating: 5,
     },
   ];
 
